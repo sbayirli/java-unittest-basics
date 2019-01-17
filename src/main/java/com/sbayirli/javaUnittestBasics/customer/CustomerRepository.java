7896b0f652f6851ed66ca787a7834161629fe4e1
Sibel BAYIRLI <sibelbayirli@Sibels-MacBook-Pro.local>
2019-01-17 23:45:52 +0300

    customer service is loading

--- src/main/java/com/sbayirli/javaUnittestBasics/customer/CustomerRepository.java
+++ src/main/java/com/sbayirli/javaUnittestBasics/customer/CustomerRepository.java
@@ -1,4 +1,7 @@
 package com.sbayirli.javaUnittestBasics.customer;
 
 public class CustomerRepository {
+    public void saveCustomer(Customer customer) {
+        System.out.println("Customer is being saved");
+    }
 }
---
+++ src/test/java/com/sbayirli/javaUnittestBasics/helloTest/CustomerServiceTest.java
@@ -0,0 +1,8 @@
+package com.sbayirli.javaUnittestBasics.helloTest;
+
+/**
+ * Created by sibelbayirli on 1/17/19.
+ */
+public class CustomerServiceTest {
+
+}
