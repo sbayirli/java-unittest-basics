5912aa1a671ed29ef6abb77e687ce8f60dc8eae0
Sibel Bayırlı <sbayirli@muhasebat.gov.tr>
2019-01-17 14:19:30 +0300

    First Commit

---
+++ src/main/java/com/sbayirli/javaUnittestBasics/JavaUnittestBasicsApplication.java
@@ -0,0 +1,14 @@
+package com.sbayirli.javaUnittestBasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaUnittestBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaUnittestBasicsApplication.class, args);
+	}
+
+}
+
